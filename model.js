03eb7f3f898c9feebb7a8281016d5175e0285c2b
Ryan Birmingham <birm@rbirm.us>
2018-06-19 16:41:06 -0400

    Update model.js

--- model.js
+++ model.js
@@ -1,3 +1,7 @@
 const model = tf.sequential();
 model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+imgtensors = imgtoTensors()
+model.fit(imgtensors[0], imgtensors[1], {epochs: 10}).then(()=>{
+  model.predict(tf.tensor1d([100,200])).print();
+});
