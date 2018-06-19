90d1c75446498d0a50ff7397c4b18635d94b5f22
Ryan Birmingham <birm@rbirm.us>
2018-06-19 16:38:00 -0400

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,19 +1,22 @@
-function rangetoTensors(x,y,w,h){
+// create a list of [x,y] tensors and a list of [r,g,b,a] tensors from an image
+function rangeToTensors(x,y,w,h){
   var imgdata = ctx.getImageData(x,y,w,h).data
   imgdata = Array.from(imgdata);
-  tensors=[]
+  coords=[]
+  datas=[]
   for (var i=0; i<w*h; i+=4){
-    let data = [i%(w), Math.floor(i/w)]
-    data.concat(imgdata.slice(i,i+4))
-    tensors.push(tf.tensor1d(data))
+    let coord = [i%(w), Math.floor(i/w)]
+    let data =imgdata.slice(i,i+4)
+    datas.push(tf.tensor1d(data))
+    coords.push(tf.tensor1d(coord))
   }
   return tensors;
 }
 
-function ImgtoTensors(){
+function imgtoTensors(){
   var x = 0;
   var y = 0;
   var w = canvas.width;
   var h = canvas.height;
-  return rangetoTensors(x,y,w,h)
+  return rangeToTensors(x,y,w,h)
 }
