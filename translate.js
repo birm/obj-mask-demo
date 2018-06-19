2e98a2f725b59c7f944014b96e891cd1d4cd3696
Ryan Birmingham <birm@rbirm.us>
2018-06-19 16:28:41 -0400

    Update translate.js

--- translate.js
+++ translate.js
@@ -1,13 +1,19 @@
-function rangetoTensor(x,y,w,h){
+function rangetoTensors(x,y,w,h){
   var imgdata = ctx.getImageData(x,y,w,h).data
   imgdata = Array.from(imgdata);
-  return tf.tensor2d(imgdata, [4,w*h])
+  tensors=[]
+  for (var i=0; i<w*h; i+=4){
+    let data = [i%(w), Math.floor(i/w)]
+    data.concat(imgdata.slice(i,i+4))
+    tensors.push(tf.tensor1d(data))
+  }
+  return tensors;
 }
 
-function ImgtoTensor(){
+function ImgtoTensors(){
   var x = 0;
   var y = 0;
   var w = canvas.width;
   var h = canvas.height;
-  return rangetoTensor(x,y,w,h)
+  return rangetoTensors(x,y,w,h)
 }
